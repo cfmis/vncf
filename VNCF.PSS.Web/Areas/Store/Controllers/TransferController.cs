4e2c39c9fa7912ceb4caf50854209de0945f9d55
cfmis <leavy_lai@chingfung.com>
2021-03-16 15:54:35 +0800

    Add Store On 2021/03/16

---
+++ VNCF.PSS.Web/Areas/Store/Controllers/TransferController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VNCF.PSS.Web.Areas.Store.Controllers
+{
+    public class TransferController : Controller
+    {
+        // GET: Store/Transfer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
